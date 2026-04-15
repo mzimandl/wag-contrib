a0b8c2d2dd142810477f5db0a6e37034ffbb6ddf
Martin Zimandl <martin.zimandl@gmail.com>
2026-04-15 11:36:33 +0200

    wip rich query match

---
+++ src/js/tiles/custom/lexOverview/lexQueryMatch.ts
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2026 Martin Zimandl <martin.zimandl@gmail.com>
+ * Copyright 2026 Institute of the Czech National Corpus,
+ *                Faculty of Arts, Charles University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { QueryMatch } from '../../../query/index.js';
+
+enum LexSource {
+    ASSC = 'assc',
+    IJP = 'ijp',
+    SSJC = 'ssjc',
+    SJC = 'sjc',
+}
+
+enum PoS {
+    ADJ = 'A',
+    ABB = 'B',
+    NUM = 'C',
+    ADV = 'D',
+    FORE = 'F',
+    INTER = 'I',
+    CONJ = 'J',
+    NOUN = 'N',
+    PRON = 'P',
+    PREP = 'R',
+    SEGM = 'S',
+    PART = 'T',
+    VERB = 'V',
+    UNKN = 'X',
+    PUNC = 'Z',
+}
+
+enum Gender {
+    MASCULINE_ANIM = 'M',
+    MASCULINE_INAN = 'I',
+    FEMININE = 'F',
+    NEUTER = 'N',
+}
+
+interface LexItem {
+    idents: Array<string>;
+    source: LexSource;
+
+    lemma: string;
+    pos: PoS;
+    gender?: Gender;
+
+    corpusEntry?: QueryMatch<undefined>;
+    extraSources: { [source: string]: Array<string> };
+}
+
+interface LexExtraData {
+    mainSource: LexSource;
+    lexItems: Array<LexItem>;
+}
+
+export function isLexQueryMatch(
+    qm: QueryMatch<any>
+): qm is QueryMatch<LexExtraData> {
+    return (
+        qm.extraData !== undefined &&
+        (qm.extraData as LexExtraData).mainSource !== undefined
+    );
+}
